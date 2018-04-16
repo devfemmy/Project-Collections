0cb3c4fc329cdcd5e366b265ee71039f261650da
odoyoakereleisrael <38425489+odoyoakereleisrael@users.noreply.github.com>
2018-04-16 12:03:32 +0100

    tite.php
    HTML firstpage

---
+++ tite.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+<title>figma test</title>
+<style type="text/css">
+body {background-color:grey;
+    }
+div{width: 250px;margin-left: 40%;margin-top: 10%;font-size: 24;}
+p{width: 250px;margin-left: 45%;margin-top: 2%;font-size: 12,}
+date{color: blue;}
+</style>
+</head>
+<body>
+<div id="title">HNG INTERNSHIP 4.O</div>
+<p><?php
+echo "".   date("h:ia");
+?></p>
+
+</body>
+</html>
